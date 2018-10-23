779dd2ca8fdac251ec3382ed5bf8c832d0c16521
Lovemefan <450489712@qq.com>
2018-10-23 16:16:42 +0800

    的格式符合 (#1)
    * 测试
    
    * 测试

--- app/src/main/java/com/bignerdranch/android/geoquiz/Question.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/Question.java
@@ -7,6 +7,7 @@ package com.bignerdranch.android.geoquiz;
 public class Question {
     private int mTextResId;
     private boolean mAnswerTrue;
+    private String test ="hello";
 
     public Question(int textResId,boolean answerTrue){
         mAnswerTrue=answerTrue;
